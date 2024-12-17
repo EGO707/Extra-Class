3786bbf312e8a41b107d0e3b9f85f8d6f1a85c7a
EGO707 <143206829+EGO707@users.noreply.github.com>
2024-12-17 22:44:57 +0530

    Update courses_data.py

--- courses_data.py
+++ courses_data.py
@@ -1,22 +1,29 @@
 # data mainly used in search results 
-
 COURSES = {
     'cs': {
         1: ['Programming for Problem Solving', 'Mathematics I'],
-        2 : ['Mathematics 2','Fundamental of Mechanical engineering'],
-        3: ['Data Structures', 'Mathematics 3','Digital Electronics'],
-        # Add more sem
+        2: ['Mathematics 2', 'Fundamental of Mechanical engineering'],
+        3: ['Data Structures', 'Mathematics 3', 'Digital Electronics'],
+        # Add more semesters
     },
     'electronics': {
         1: ['Basic Electronics', 'Physics'],
-        2 : ['Mathematics ll', 'Fundamental of electrical engineering'],
-        3 : ['Digital system design','Electronic Devices'],
-        # Add more sem
+        2: ['Mathematics ll', 'Fundamental of electrical engineering'],
+        3: ['Digital system design', 'Electronic Devices'],
+        # Add more semesters
     },
     'electrical': {
         1: ['Soft skills', 'Mathematics I'],
-        2 : ['Environment and ecology'],
-        3 : ['Electromagnetic Theory','Cybersecurity'],
-        # Add more sem
+        2: ['Environment and ecology'],
+        3: ['Electromagnetic Theory', 'Cybersecurity'],
+        # Add more semesters
     }
 }
+
+PDF_MAPPING = {}
+
+# Add "data_structures.pdf" for every subject
+for course in COURSES.values():
+    for sem_subjects in course.values():
+        for subject in sem_subjects:
+            PDF_MAPPING[subject] = "example.pdf"
